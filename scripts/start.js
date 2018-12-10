a7a1902403734feab9324d9a7a427194e27ff5ac
DOGGA Nidhal <dogga.nidhal@gmail.com>
2018-12-10 02:25:51 +0100

    fix start script logs

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,5 @@
 
-let {execSync} = require("child_process");
+let {exec} = require("child_process");
 let config = require('../config');
 let service = process.env.SERVICE;
 let nodeEnv = process.env.NODE_ENV;
@@ -10,4 +10,12 @@ if (!npmCommand) {
     console.error("Can't parse npm to start from config.json");
 }
 
-execSync(`npm run ${npmCommand}`);
+let executable = exec(`npm run ${npmCommand}`, error => {
+    if (error) 
+     console.error(error);
+});
+
+executable.stdout.on("data", data => console.log(data));
+
+executable.stderr.on("error", error => console.error(error));
+executable.stderr.on("data", data => console.error(data));
