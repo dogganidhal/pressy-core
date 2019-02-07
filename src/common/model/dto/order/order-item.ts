7ade4a000ea8960a828745b24f6aa15a5bdfada8
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-02-07 08:21:51 +0100

    fix order item dto

--- src/common/model/dto/order/order-item.ts
+++ src/common/model/dto/order/order-item.ts
@@ -4,12 +4,14 @@ import { OrderItem } from "../../entity/order/order-item";
 
 export class OrderItemDto {
 
+	public id: number;
 	public orderId: number;
 	public article: ArticleDto;
 	public color: string;
 	public comment?: string;
 
 	constructor(item: OrderItem) {
+		this.id = item.id;
 		this.orderId = item.order.id;
 		this.article = new ArticleDto(item.article);
 		this.color = item.color;
