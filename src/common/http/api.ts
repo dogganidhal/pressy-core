b03be945506837e32ca8fd9d8fc7a862bc471df5
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-06-06 19:29:44 +0100

    add allowed methods to cors headers

--- src/common/http/api.ts
+++ src/common/http/api.ts
@@ -33,6 +33,7 @@ export class APIV1 {
 		this._express.use(function (req, res, next) {
 			res.header("Access-Control-Allow-Origin", "*");
 			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 			next();
 		});
 		
